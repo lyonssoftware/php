d4c21c60ed77846b32b665b82f67a258f5341192
Lyons Software <admin@lyonssoftware.com>
2016-06-01 23:14:12 -0500

    Update constants.php

--- constants.php
+++ constants.php
@@ -1,4 +1,5 @@
 <?php
+  // consstants are designed to not change
   // constants hold a variable that is not meant to be changed?, they don't change 
   // create a constant, website, that holds string 
   define("website", "This is github.com/lyonssoftware");
