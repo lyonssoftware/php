c74668e8182a7e87b9e77c1a4f89599070fecaab
Lyons Software <admin@lyonssoftware.com>
2016-06-30 22:18:25 -0500

    Create queueShift.php

---
+++ queueShift.php
@@ -0,0 +1,20 @@
+<?php 
+function processSendQueue($socket, $sendQueue) { 
+    while (!$sendQueue->isEmpty()) { 
+                            //shift() is the same as dequeue() 
+        $senditem = $sendQueue->shift(); 
+
+        //returns the number of bytes written. 
+        $rtn = socket_write($socket, $senditem); 
+        if ($rtn === false) { 
+            $sendQueue->unshift($senditem); 
+            throw new exception("send error: " . socket_last_error($socket)); 
+            return; 
+        } 
+        if ($rtn < strlen($senditem) { 
+            $sendQueue->unshift(substr($senditem, $rtn); 
+            break; 
+        } 
+    } 
+} 
+?>
