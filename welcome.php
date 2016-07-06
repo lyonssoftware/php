12e0e52c968aa5efab813598ab08a6c8d527bbeb
Lyons Software <admin@lyonssoftware.com>
2016-07-05 23:41:57 -0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>  
 <body>
-
+<!-- USE THE FORM TO GET INFO -->
 <form action="welcome.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
