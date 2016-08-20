fd04c0cb53145d5f43fcb784a35b752c73a90bf3
Lyons Software <admin@lyonssoftware.com>
2016-08-19 21:52:20 -0500

    Update foreach.php

--- foreach.php
+++ foreach.php
@@ -5,6 +5,7 @@
   // a modified for loop, foreach loops are used  to iterate through all colors in array 
   foreach ($colors as $value) 
   {
+    // print the current color 
     echo "$value <br>";
   }
 ?> 
