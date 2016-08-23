999ffc8e1f5faa456a2b07fa7cb9b7069c507743
Lyons Software <admin@lyonssoftware.com>
2016-08-22 23:29:30 -0500

    Update constants.php

--- constants.php
+++ constants.php
@@ -1,5 +1,6 @@
 <?php
   // create a constant, website, that holds string 
+  // constants are very useful if you know a variable will not change
   define("website", "This is github.com/lyonssoftware");
   echo website;
 ?> 
