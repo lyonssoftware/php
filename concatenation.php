86fd00da9fa26ad08aedd0913d4e3f488617c186
Lyons Software <admin@lyonssoftware.com>
2016-09-04 23:41:08 -0700

    Update concatenation.php

--- concatenation.php
+++ concatenation.php
@@ -1,6 +1,5 @@
 <?php
-  // concat or concatenation 
-  // concatenation combines string txt2 to txt1 with the  "." symbol 
+  // . which is an alternative for concat() concatenation combines string txt2 to txt1 with the  "." symbol 
   $txt1 = "Hello ";
   $txt2 = " world!";
   echo $txt1 . $txt2;
