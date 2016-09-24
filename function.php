98cbdb99a2ef7f6d5949ba1cf6e8e8ea3c191360
Lyons Software <admin@lyonssoftware.com>
2016-09-23 20:50:11 -0500

    Update function.php

--- function.php
+++ function.php
@@ -5,6 +5,7 @@ $b=13;
 // functions perform a list of steps in order; perform the function add() with two parameters
 function add($a,$b){
     echo "The Sum of $a+$b= ", $a+$b, "<br />";
+    // variables 
     $a=5;
     $b=10;
     echo "The sum is=" ,$a+$b , "<br />" ;    }
