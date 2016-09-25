d1bad681789f28827bc56893d868413b9550fdc9
Lyons Software <admin@lyonssoftware.com>
2016-09-24 22:02:20 -0500

    Update descendingSort.php

--- descendingSort.php
+++ descendingSort.php
@@ -1,5 +1,5 @@
 <?php
-  // the array of integers
+  // the array of integers that will be sorted 
   $numbers = array(4, 6, 2, 22, 11);
   
   // rsort() sorts the array in descending. The new order will be  22,11,6,4,2
