2dd06a530a33748f13778e21eecd71692dc3d7ea
Lyons Software <admin@lyonssoftware.com>
2016-09-25 22:08:40 -0700

    Update dowhileloop.php

--- dowhileloop.php
+++ dowhileloop.php
@@ -1,6 +1,6 @@
 
 <?php
-// the first part of the do statement will be perform once, then the while loop  
+// the statement will execute once, then the while loop  
 do {
     if ($i < 5) {
         echo "i is not big enough";
